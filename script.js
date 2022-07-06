aa8d4026ce4d3159b198c11bba7e2b631f1cf648
Darya Korenko <darya.korenko@mail.ru>
2022-07-06 19:57:29 +0300

    добавила пару файлов

---
+++ script.js
@@ -0,0 +1,20 @@
+'use-strict'
+
+const title = document.getElementById('title');
+
+const myAge = 1; // Number
+const myName = 'Daruja'; // String
+const isGirl = true; // Boolean
+const avocadoCount = null;
+
+const catNames = ['Murisa'];
+const mainCat = {
+  name: 'Murisa',
+  age: 7,
+};
+
+let dogName = 'Dmitry'; // declaration / объявление
+
+dogName = 'Bobik'; // expression 
+
+console.log(1 === 1); // expression / выражение
