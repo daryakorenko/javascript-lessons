0a7849494cfad6ca1698d4ac9d0e29a7ebcd8053
Darya Korenko <darya.korenko@mail.ru>
2022-07-10 10:26:59 +0300

    Писала конспект

--- script.js
+++ script.js
@@ -7,14 +7,37 @@ const myName = 'Daruja'; // String
 const isGirl = true; // Boolean
 const avocadoCount = null;
 
-const catNames = ['Murisa'];
+const catNames = ['Murisa', 'Mijuki'];
 const mainCat = {
   name: 'Murisa',
   age: 7,
 };
 
-let dogName = 'Dmitry'; // declaration / объявление
+// let dogName = 'Dmitry'; // declaration / объявление
+// dogName = 'Bobik'; // statement / утверждение, инструкция
+// console.log(dogName);
+// console.log(`Привет, меня зовут ${dogName}`);
 
-dogName = 'Bobik'; // expression 
+// Простой способ объявить функцию
+function pogladitKota(catName, catBreed) {
+  // console.log(`Мою кошку зовут ${catName}! И она - ${catBreed} 🐱`);
+  return `Мою кошку зовут ${catName}! И она - ${catBreed} 🐱`;
+  // const text = `Мою кошку зовут ${catName}! И она - ${catBreed} 🐱`;
+  // document.getElementById('title').innerHTML = text;
+}
 
-console.log(1 === 1); // expression / выражение
+const myCatInfo = pogladitKota('Мурыса', 'турецкая булка');
+
+function showMyCatInfo() {
+  document.getElementById('forms').innerHTML = myCatInfo;
+}
+
+// Еще один способ объявить функцию
+const pogladitSobaku = function () {
+  console.log('Кто хороший пёс!');
+};
+
+// Вызываем функцию
+// pogladitSobaku();
+// pogladitKota('Мурыса', 'турецкая ангора');
+// pogladitKota('Бука', 'монстр с изнанки');
